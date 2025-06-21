1019988d10eb95fbf25e56f14ea811f9790abb7d
SyedUbada19 <syedubadaofficials@gmail.com>
2025-06-22 00:45:56 +0530

    chore: configure for Netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
-  // Forcing a new commit
+  output: 'static',
+  // No adapter needed for a standard static build
 });
