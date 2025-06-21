436c6e46f485345d329cbb40be86f955bf41f828
SyedUbada19 <syedubadaofficials@gmail.com>
2025-06-22 00:24:29 +0530

    feat: prepare for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://saadpasta.github.io',
+  adapter: vercel(),
 });
